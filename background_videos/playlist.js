48cde014a20e7c4359df3fbdd56b19aa97706ccf
Nic <nic.haegele08@gmail.com>
2026-07-08 19:58:39 +0200

    Added new things

---
+++ background_videos/playlist.js
@@ -0,0 +1,8 @@
+window.NIC_BACKGROUND_VIDEOS = [
+  "background_videos/1000028294.MP4",
+  "background_videos/1000028298.MP4",
+  "background_videos/1000031307.MP4",
+  "background_videos/dji_fly_20230929_162228_320_1696002850314_quickshot_cache.MP4",
+  "background_videos/dji_fly_20250815_195912_462_1771604042247_video.MP4",
+  "background_videos/dji_fly_20250816_192528_482_1771603185225_quickshot.MP4"
+];
